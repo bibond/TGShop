02c978993ba788a2d0f2a2125726b60a014c8a12
bibond <hoangbaothien1357@gmail.com>
2018-05-30 11:35:44 +0700

    Add models Catgory - Product - User

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+for i in 0..5
+  category = Category.create(title: 'Category ' + i.to_s, tag: 0)
+
+  for j in 1..5
+    index_product = i * 10 + j
+    product = Product.create(name: 'Product ' + index_product.to_s, content: 'Content here.', category_id: category.id)
+  end
+end
